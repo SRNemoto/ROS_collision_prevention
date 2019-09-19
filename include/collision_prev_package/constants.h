344879f3a8dae58f8e8b4bc0a878ef7b9be12fc9
Shota Nemoto <srn24@case.edu>
2019-09-19 00:50:39 -0400

    Added launch files and constants file

---
+++ include/collision_prev_package/constants.h
@@ -0,0 +1,25 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define _USE_MATH_DEFINES
+#include <string>
+#include <math.h>
+
+std::string laser_topic("laser_1");
+
+// Default input velocity topic name "des_vel"
+std::string input_vel_topic("des_vel");
+
+// Default output topic name "cmd_vel"
+std::string output_vel_topic("cmd_vel");
+
+const float ROBOT_RADIUS = 0.15; // meters
+
+const float MIN_DISTANCE = 0.1; //meters
+
+const float MIN_LASER_READING = ROBOT_RADIUS + MIN_DISTANCE; // minimum lidar reading in meters
+
+// Angle converted to radians and converted to integer
+const int ANGLE_MARGIN = static_cast<int>(ceil(tan(ROBOT_RADIUS / (MIN_LASER_READING)) * (180 / M_PI)));
+
+#endif
